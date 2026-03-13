c65cb72524cbc036eefe40ebebc617d471fe5be5
vale-rg02 <valeria.rg.24@gmail.com>
2026-03-12 17:45:00 -0700

    listadoproducto

--- SistemaGym/FrmListadoProducto.Designer.cs
+++ SistemaGym/FrmListadoProducto.Designer.cs
@@ -29,85 +29,160 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.dgvproductos = new System.Windows.Forms.DataGridView();
+            this.btnnuevo = new System.Windows.Forms.Button();
+            this.btneditar = new System.Windows.Forms.Button();
+            this.btneliminar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnbuscar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label2 = new System.Windows.Forms.Label();
             this.radioButton1 = new System.Windows.Forms.RadioButton();
             this.radioButton2 = new System.Windows.Forms.RadioButton();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproductos)).BeginInit();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(45, 36);
+            this.label1.Location = new System.Drawing.Point(26, 28);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(131, 29);
             this.label1.TabIndex = 0;
             this.label1.Text = "Productos";
             this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
+            // dgvproductos
+            // 
+            this.dgvproductos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvproductos.Location = new System.Drawing.Point(31, 211);
+            this.dgvproductos.Name = "dgvproductos";
+            this.dgvproductos.RowHeadersWidth = 51;
+            this.dgvproductos.RowTemplate.Height = 24;
+            this.dgvproductos.Size = new System.Drawing.Size(749, 227);
+            this.dgvproductos.TabIndex = 7;
+            // 
+            // btnnuevo
+            // 
+            this.btnnuevo.Location = new System.Drawing.Point(495, 452);
+            this.btnnuevo.Name = "btnnuevo";
+            this.btnnuevo.Size = new System.Drawing.Size(91, 23);
+            this.btnnuevo.TabIndex = 8;
+            this.btnnuevo.Text = "&Nuevo";
+            this.btnnuevo.UseVisualStyleBackColor = true;
+            // 
+            // btneditar
+            // 
+            this.btneditar.Location = new System.Drawing.Point(592, 452);
+            this.btneditar.Name = "btneditar";
+            this.btneditar.Size = new System.Drawing.Size(91, 23);
+            this.btneditar.TabIndex = 9;
+            this.btneditar.Text = "&Editar";
+            this.btneditar.UseVisualStyleBackColor = true;
+            // 
+            // btneliminar
+            // 
+            this.btneliminar.Location = new System.Drawing.Point(689, 452);
+            this.btneliminar.Name = "btneliminar";
+            this.btneliminar.Size = new System.Drawing.Size(91, 23);
+            this.btneliminar.TabIndex = 10;
+            this.btneliminar.Text = "&Eliminar";
+            this.btneliminar.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(208, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(378, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // btnbuscar
+            // 
+            this.btnbuscar.Location = new System.Drawing.Point(592, 129);
+            this.btnbuscar.Name = "btnbuscar";
+            this.btnbuscar.Size = new System.Drawing.Size(91, 23);
+            this.btnbuscar.TabIndex = 5;
+            this.btnbuscar.Text = "&Buscar";
+            this.btnbuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Location = new System.Drawing.Point(689, 129);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(91, 23);
+            this.btnsalir.TabIndex = 6;
+            this.btnsalir.Text = "&Salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(24, 102);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(178, 72);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Criterio de Búsqueda";
+            this.groupBox1.UseWaitCursor = true;
+            // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(47, 92);
+            this.label2.Location = new System.Drawing.Point(22, 3);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(133, 16);
+            this.label2.Size = new System.Drawing.Size(0, 16);
             this.label2.TabIndex = 1;
-            this.label2.Text = "Criterio de Búsqueda";
+            this.label2.UseWaitCursor = true;
             // 
             // radioButton1
             // 
             this.radioButton1.AutoSize = true;
-            this.radioButton1.Location = new System.Drawing.Point(50, 129);
+            this.radioButton1.Location = new System.Drawing.Point(10, 32);
             this.radioButton1.Name = "radioButton1";
             this.radioButton1.Size = new System.Drawing.Size(77, 20);
             this.radioButton1.TabIndex = 2;
             this.radioButton1.TabStop = true;
             this.radioButton1.Text = "Nombre";
             this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.UseWaitCursor = true;
             // 
             // radioButton2
             // 
             this.radioButton2.AutoSize = true;
-            this.radioButton2.Location = new System.Drawing.Point(133, 129);
+            this.radioButton2.Location = new System.Drawing.Point(93, 32);
             this.radioButton2.Name = "radioButton2";
-            this.radioButton2.Size = new System.Drawing.Size(93, 20);
+            this.radioButton2.Size = new System.Drawing.Size(72, 20);
             this.radioButton2.TabIndex = 3;
             this.radioButton2.TabStop = true;
-            this.radioButton2.Text = "IdProducto";
+            this.radioButton2.Text = "Código";
             this.radioButton2.UseVisualStyleBackColor = true;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(252, 127);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(378, 22);
-            this.textBox1.TabIndex = 4;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(636, 126);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(91, 23);
-            this.button1.TabIndex = 5;
-            this.button1.Text = "Buscar";
-            this.button1.UseVisualStyleBackColor = true;
+            this.radioButton2.UseWaitCursor = true;
             // 
             // FrmListadoProducto
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(824, 491);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btneliminar);
+            this.Controls.Add(this.btneditar);
+            this.Controls.Add(this.btnnuevo);
+            this.Controls.Add(this.dgvproductos);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnbuscar);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.radioButton2);
-            this.Controls.Add(this.radioButton1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "FrmListadoProducto";
             this.Text = "FrmListadoProducto";
             this.Load += new System.EventHandler(this.FrmListadoProducto_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvproductos)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -116,10 +191,16 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.RadioButton radioButton1;
-        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.DataGridView dgvproductos;
+        private System.Windows.Forms.Button btnnuevo;
+        private System.Windows.Forms.Button btneditar;
+        private System.Windows.Forms.Button btneliminar;
         private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnbuscar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label2;
     }
 }
